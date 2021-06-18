bb811b409be7a94e67c7dcf6c7a82d10509f8438
shrekzz <553238045@qq.com>
2021-06-18 15:18:29 +0800

    add recommend

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -3,6 +3,8 @@
     <home-header />
     <home-swiper />
     <home-icons />
+    <home-recommend />
+    <home-weekend />
   </div>
 </template>
 
@@ -10,6 +12,8 @@
 import HomeHeader from "./components/Header.vue";
 import HomeSwiper from "./components/Swiper.vue";
 import HomeIcons from "./components/Icons.vue";
+import HomeRecommend from "./components/Recommend.vue";
+import HomeWeekend from "./components/Weekend.vue";
 
 export default {
   naem: "Home",
@@ -17,6 +21,8 @@ export default {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
+    HomeRecommend,
+    HomeWeekend,
   },
 };
 </script>
